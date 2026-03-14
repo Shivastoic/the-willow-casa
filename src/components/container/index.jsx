e6fa219c1717e31828583af5b36c20a20e0b6d11
Shiva pandey <107467698+Shivastoic@users.noreply.github.com>
2026-03-14 15:45:16 +0530

    Initial Next.js project scaffold
    Add initial project scaffold for a Next.js app: .gitignore, package.json and package-lock.json, ESLint and PostCSS configs, jsconfig and next.config, global styles, and basic app files (src/app/layout.js, src/app/page.js) plus simple components (header, footer, container). This provides a starter setup with Next.js, React, Tailwind-related configs and path aliases to begin development.

---
+++ src/components/container/index.jsx
@@ -0,0 +1,11 @@
+export default function Container({ children }) {
+
+    return (
+
+        <section className="mx-auto px-6 lg:max-w-300 h-full">
+            { children }
+        </section>
+
+    )
+
+}
